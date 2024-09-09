4f39b8e679c233b344f264c44da20a3a2c5092a1
Amadou Cisse <cisse.amadou.9@gmail.com>
2024-09-09 17:47:36 +0200

    clean up

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
 // Main
 //> using scala "3"
-//> using options "-deprecation"
+//> using options "-Xfatal-warnings" "-deprecation" "-encoding" "-source" "-unchecked" "UTF-8" "future"
 //> using toolkit "org.typelevel:0.1.27"
 
